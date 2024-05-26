66bd2a887e4be016fb4339a5c7dcec6e61ff42d4
Илья <149706253+4URKa0@users.noreply.github.com>
2024-05-26 12:34:31 +0300

    delete

--- routes/game.js
+++
@@ -1,9 +0,0 @@
-const gamesRouter = require("express").Router(); 
-const { sendAllGames,deleteGame,addGameController } = require("../controllers/games");
-const { getAllGames } = require('../middlewares/games')
-
-gamesRouter.get("/games", getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
-gamesRouter.post("/games", getAllGames, addGameController)
-
-module.exports = gamesRouter;
