1533bac54687f2e95a58da9a0ee9351c2bc0e6f6
Илья <149706253+4URKa0@users.noreply.github.com>
2024-05-27 21:44:12 +0300

    обновил мидлвары

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,15 +1,16 @@
-function cors(req, res, next) {
-    const { origin } = req.headers;
-    
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    
-    next();
-}
-
 const allowedCors = [
   'https://practicum.yandex.ru',
   'https://students-projects.ru',
-  'http://localhost:3000'
+  'localhost:3000'
 ];
+
+function cors(req, res, next) {
+  const { origin } = req.headers; 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+};
+
+module.exports = cors;
